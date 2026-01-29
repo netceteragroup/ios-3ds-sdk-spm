2819b423897672298c1d7708a1b7ae76aca4901e
hristijan-anastasovski <158057428+hanastas1@users.noreply.github.com>
2026-01-29 16:48:05 +0100

    Release 2.5.30

--- Package.swift
+++ Package.swift
@@ -17,8 +17,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .binaryTarget(
             name: "ThreeDS_SDK",
-            url: "https://nexus.extranet.netcetera.biz/nexus/repository/public-repository-maven/com/netcetera/nca-341-2/3ds-sdk/ios/release/2.5.22/ThreeDS_SDK.zip",
-            checksum: "8ed06e2a903cd04c98fb450a2a44788f18aeaa3f8a108ffe795b9526e34248e6"
+            url: "https://nexus.extranet.netcetera.biz/nexus/repository/public-repository-maven/com/netcetera/nca-341-2/3ds-sdk/ios/release/2.5.30/ThreeDS_SDK.zip",
+            checksum: "01fd1cb91f7c649e90b35fc94825c8b7434342fb8af20ac7a6d7f55f9162d3a0"
         )
     ]
 )
