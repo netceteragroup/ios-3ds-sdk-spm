3cdaa8dc7d157a11f69a6617a5dea5a41ec63898
hristijan-anastasovski <158057428+hanastas1@users.noreply.github.com>
2026-01-29 15:59:25 +0100

    Release 2.4.00

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "ThreeDS_SDK",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "ThreeDS_SDK",
+            targets: ["ThreeDS_SDK"])
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .binaryTarget(
+            name: "ThreeDS_SDK",
+            url: "https://nexus.extranet.netcetera.biz/nexus/repository/public-repository-maven/com/netcetera/nca-341-2/3ds-sdk/ios/release/2.4.00/ThreeDS_SDK.zip",
+            checksum: "a92ebbdd2350484c21d4560503fe1b3a0f72d6fc477159158f0d69daf0ca66c5"
+        )
+    ]
+)
